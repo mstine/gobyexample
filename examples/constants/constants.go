7659901ceb8fcb84311ff1f838a220c589cc6282
Matt Stine <mstine@gopivotal.com>
2015-03-03 22:49:48 -0600

    modify for slides

--- examples/constants/constants.go
+++ examples/constants/constants.go
@@ -7,27 +7,20 @@ import "fmt"
 import "math"
 
 // `const` declares a constant value.
+// START OMIT
 const s string = "constant"
 
 func main() {
-    fmt.Println(s)
+	fmt.Println(s)
 
-    // A `const` statement can appear anywhere a `var`
-    // statement can.
-    const n = 500000000
+	const n = 500000000
 
-    // Constant expressions perform arithmetic with
-    // arbitrary precision.
-    const d = 3e20 / n
-    fmt.Println(d)
+	const d = 3e20 / n
+	fmt.Println(d)
 
-    // A numeric constant has no type until it's given
-    // one, such as by an explicit cast.
-    fmt.Println(int64(d))
+	fmt.Println(int64(d))
 
-    // A number can be given a type by using it in a
-    // context that requires one, such as a variable
-    // assignment or function call. For example, here
-    // `math.Sin` expects a `float64`.
-    fmt.Println(math.Sin(n))
+	fmt.Println(math.Sin(n))
 }
+
+// END OMIT
