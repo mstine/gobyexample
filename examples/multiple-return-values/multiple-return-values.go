e01c009f6cc8a4369b362635225453d219322244
Matt Stine <mstine@gopivotal.com>
2015-03-05 17:59:58 -0600

    modify for slides

--- examples/multiple-return-values/multiple-return-values.go
+++ examples/multiple-return-values/multiple-return-values.go
@@ -6,25 +6,21 @@ package main
 
 import "fmt"
 
-// The `(int, int)` in this function signature shows that
-// the function returns 2 `int`s.
+// START OMIT
 func vals() (int, int) {
-    return 3, 7
+	return 3, 7
 }
 
 func main() {
+	a, b := vals()
+	fmt.Println(a)
+	fmt.Println(b)
 
-    // Here we use the 2 different return values from the
-    // call with _multiple assignment_.
-    a, b := vals()
-    fmt.Println(a)
-    fmt.Println(b)
-
-    // If you only want a subset of the returned values,
-    // use the blank identifier `_`.
-    _, c := vals()
-    fmt.Println(c)
+	_, c := vals()
+	fmt.Println(c)
 }
+
+// END OMIT
 
 // todo: named return parameters
 // todo: naked returns
