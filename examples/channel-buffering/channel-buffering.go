f695fd66a6e6a366a2215be859f5ef4a9fafd61d
Matt Stine <mstine@gopivotal.com>
2015-03-05 22:53:11 -0600

    modify for slides

--- examples/channel-buffering/channel-buffering.go
+++ examples/channel-buffering/channel-buffering.go
@@ -10,18 +10,13 @@ package main
 import "fmt"
 
 func main() {
+	// START OMIT
+	messages := make(chan string, 2)
 
-    // Here we `make` a channel of strings buffering up to
-    // 2 values.
-    messages := make(chan string, 2)
+	messages <- "buffered"
+	messages <- "channel"
 
-    // Because this channel is buffered, we can send these
-    // values into the channel without a corresponding
-    // concurrent receive.
-    messages <- "buffered"
-    messages <- "channel"
-
-    // Later we can receive these two values as usual.
-    fmt.Println(<-messages)
-    fmt.Println(<-messages)
+	fmt.Println(<-messages)
+	fmt.Println(<-messages)
+	// END OMIT
 }
