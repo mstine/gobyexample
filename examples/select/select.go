f06ff7e1ec857acf496e6811b65e0673ff5ab59b
Matt Stine <mstine@gopivotal.com>
2015-03-05 22:58:13 -0600

    modify for slides

--- examples/select/select.go
+++ examples/select/select.go
@@ -8,31 +8,28 @@ import "time"
 import "fmt"
 
 func main() {
+	// START_ONE OMIT
+	c1 := make(chan string)
+	c2 := make(chan string)
 
-    // For our example we'll select across two channels.
-    c1 := make(chan string)
-    c2 := make(chan string)
+	go func() {
+		time.Sleep(time.Second * 1)
+		c1 <- "one"
+	}()
+	go func() {
+		time.Sleep(time.Second * 2)
+		c2 <- "two"
+	}()
+	// END_ONE OMIT
 
-    // Each channel will receive a value after some amount
-    // of time, to simulate e.g. blocking RPC operations
-    // executing in concurrent goroutines.
-    go func() {
-        time.Sleep(time.Second * 1)
-        c1 <- "one"
-    }()
-    go func() {
-        time.Sleep(time.Second * 2)
-        c2 <- "two"
-    }()
-
-    // We'll use `select` to await both of these values
-    // simultaneously, printing each one as it arrives.
-    for i := 0; i < 2; i++ {
-        select {
-        case msg1 := <-c1:
-            fmt.Println("received", msg1)
-        case msg2 := <-c2:
-            fmt.Println("received", msg2)
-        }
-    }
+	// START_TWO OMIT
+	for i := 0; i < 2; i++ {
+		select {
+		case msg1 := <-c1:
+			fmt.Println("received", msg1)
+		case msg2 := <-c2:
+			fmt.Println("received", msg2)
+		}
+	}
+	// END_TWO OMIT
 }
