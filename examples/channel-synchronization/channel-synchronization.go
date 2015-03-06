1860c56fe60f1635bef8b6f57c9a5bc857aa625a
Matt Stine <mstine@gopivotal.com>
2015-03-05 22:54:05 -0600

    modify for slides

--- examples/channel-synchronization/channel-synchronization.go
+++ examples/channel-synchronization/channel-synchronization.go
@@ -7,26 +7,20 @@ package main
 import "fmt"
 import "time"
 
-// This is the function we'll run in a goroutine. The
-// `done` channel will be used to notify another
-// goroutine that this function's work is done.
+// START OMIT
 func worker(done chan bool) {
-    fmt.Print("working...")
-    time.Sleep(time.Second)
-    fmt.Println("done")
+	fmt.Print("working...")
+	time.Sleep(time.Second)
+	fmt.Println("done")
 
-    // Send a value to notify that we're done.
-    done <- true
+	done <- true
 }
 
 func main() {
+	done := make(chan bool, 1)
+	go worker(done)
 
-    // Start a worker goroutine, giving it the channel to
-    // notify on.
-    done := make(chan bool, 1)
-    go worker(done)
-
-    // Block until we receive a notification from the
-    // worker on the channel.
-    <-done
+	<-done
 }
+
+// END OMIT
