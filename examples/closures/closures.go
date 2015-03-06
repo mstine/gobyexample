8dd8d3551697a88b5b46a852a9d9ac6d395ad914
Matt Stine <mstine@gopivotal.com>
2015-03-05 18:04:06 -0600

    modify for slides

--- examples/closures/closures.go
+++ examples/closures/closures.go
@@ -7,34 +7,24 @@ package main
 
 import "fmt"
 
-// This function `intSeq` returns another function, which
-// we define anonymously in the body of `intSeq`. The
-// returned function _closes over_ the variable `i` to
-// form a closure.
+// START OMIT
 func intSeq() func() int {
-    i := 0
-    return func() int {
-        i += 1
-        return i
-    }
+	i := 0
+	return func() int {
+		i += 1
+		return i
+	}
 }
 
 func main() {
+	nextInt := intSeq()
 
-    // We call `intSeq`, assigning the result (a function)
-    // to `nextInt`. This function value captures its
-    // own `i` value, which will be updated each time
-    // we call `nextInt`.
-    nextInt := intSeq()
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
+	fmt.Println(nextInt())
 
-    // See the effect of the closure by calling `nextInt`
-    // a few times.
-    fmt.Println(nextInt())
-    fmt.Println(nextInt())
-    fmt.Println(nextInt())
-
-    // To confirm that the state is unique to that
-    // particular function, create and test a new one.
-    newInts := intSeq()
-    fmt.Println(newInts())
+	newInts := intSeq()
+	fmt.Println(newInts())
 }
+
+// END OMIT
